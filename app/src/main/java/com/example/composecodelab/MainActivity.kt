e754979655b907f3408dfe91a12a880dc5d86a11
chanhypark <chanhypark@gmarket.com>
2024-01-09 09:52:44 +0900

    [CodeLab Tutorial] 앱에 이미지 추가하기

--- app/src/main/java/com/example/composecodelab/MainActivity.kt
+++ app/src/main/java/com/example/composecodelab/MainActivity.kt
@@ -3,10 +3,13 @@ package com.example.composecodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
@@ -15,6 +18,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -31,7 +37,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    GreetingText(message = "HappyBirthDay Sam", from = "from Code Pilot")
+                    GreetingImage(message = "HappyBirthDay Sam", from = "from Code Pilot")
                 }
             }
         }
@@ -41,35 +47,47 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
-    Column (
-        //GreetingText Element를 vertical을 Center로 정해주고 (부모 세로기준 가운데 위치하도록)
+    Column(
         verticalArrangement = Arrangement.Center,
-        //GreetingText와 parent view간의 padding간격을 설정해준다
-        modifier = modifier.padding(40.dp)
-    ){
+        modifier = Modifier.fillMaxSize()
+    ) {
         Text(
             text = message,
-            fontSize = 50.sp,
-            lineHeight = 50.sp,
-            //텍스트 Element의 정렬을 Center로 맞춰준다
-            textAlign = TextAlign.Center
+            fontSize = 100.sp,
+            lineHeight = 100.sp,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.padding(8.dp)
         )
         Text(
             text = from,
-            fontSize = 20.sp,
-            lineHeight = 20.sp,
-            //padding과 End정렬 적용
+            fontSize = 36.sp,
             modifier = Modifier
                 .padding(16.dp)
-                .align(alignment = Alignment.End)
+                .align(alignment = Alignment.CenterHorizontally)
         )
     }
 }
 
-@Preview(showBackground = true)
+
+@Composable
+fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) {
+    val image = painterResource(id = R.drawable.androidparty)
+    Box {
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.Crop
+        )
+        GreetingText(
+            message = message, from = from, modifier = Modifier.fillMaxSize().padding(8.dp)
+        )
+    }
+}
+
+@Preview(showBackground = false)
 @Composable
 fun BirthDayCardPreview() {
     ComposeCodelabTheme {
-        GreetingText(message = "Happy BirthDay", "From CodePilot")
+        GreetingImage(message = "HBD Sam!", from = "from CodePilot")
     }
 }
