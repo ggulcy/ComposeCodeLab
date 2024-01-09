fdd0a821ab996eeebd30a2185db425f40f1e1fde
chanhypark <chanhypark@gmarket.com>
2024-01-09 10:56:13 +0900

    [CodeLab Tutorial] Base Practice Task Manager

--- app/src/main/java/com/example/composecodelab/MainActivity.kt
+++ app/src/main/java/com/example/composecodelab/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -19,6 +20,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -41,37 +44,30 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
 @Composable
 fun MainContainer(modifier: Modifier = Modifier) {
-    Column {
-        TextContainer()
-    }
-}
-
-
-@Composable
-fun TextContainer(modifier: Modifier = Modifier) {
-    Column {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center
+    ) {
         Image(
-            painter = painterResource(id = R.drawable.bg_compose_background),
+            painter = painterResource(id = R.drawable.ic_task_completed),
             contentDescription = null,
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier.align(Alignment.CenterHorizontally)
         )
         Text(
             text = stringResource(id = R.string.title),
-            fontSize = 24.sp,
-            modifier = Modifier.padding(16.dp)
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .padding(top = 24.dp, bottom = 8.dp),
         )
         Text(
             text = stringResource(id = R.string.description),
+            fontSize = 16.sp,
             modifier = Modifier
-                .padding(start = 16.dp, end = 16.dp),
-            textAlign = TextAlign.Justify
-        )
-        Text(
-            text = stringResource(id = R.string.content),
-            modifier = Modifier.padding(16.dp).align(Alignment.End),
-            textAlign = TextAlign.Justify
+                .align(Alignment.CenterHorizontally)
         )
     }
 }
