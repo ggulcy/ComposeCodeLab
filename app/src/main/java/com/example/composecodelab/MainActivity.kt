04018fff3f01c6373ac164d23096374c134a1f6a
chanhypark <chanhypark@gmarket.com>
2024-01-09 18:19:42 +0900

    [CodeLab Tutorial] 상호작용 Dice앱

--- app/src/main/java/com/example/composecodelab/MainActivity.kt
+++ app/src/main/java/com/example/composecodelab/MainActivity.kt
@@ -3,36 +3,24 @@ package com.example.composecodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.MailOutline
-import androidx.compose.material.icons.filled.Phone
-import androidx.compose.material.icons.filled.Share
-import androidx.compose.material3.Icon
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.composecodelab.ui.theme.ComposeCodelabTheme
 
 class MainActivity : ComponentActivity() {
@@ -44,7 +32,7 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
                 ) {
-
+                    DiceImageAndButton()
                 }
             }
         }
@@ -52,112 +40,44 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Container() {
+fun DiceImageAndButton(modifier: Modifier = Modifier) {
     Box(
-        Modifier
-            .fillMaxSize()
-            .background(colorResource(id = R.color.quad1))
+        Modifier.fillMaxSize()
     ) {
-        MainSection(
+        var result by remember { mutableStateOf(1) }
+        val imgRes = when (result) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        //뒤의 동작에서 result값이 변함에 따라 imgRes도 재정의 된다.
+        Column(
             modifier = Modifier
-                .fillMaxWidth()
                 .align(Alignment.Center)
-                .padding(bottom = 100.dp)
-        )
-        InfoSection(
-            modifier = Modifier
-                .fillMaxWidth()
-                .align(Alignment.BottomCenter)
-                .padding(bottom = 50.dp)
-        )
+        ) {
+            Image(
+                painter = painterResource(id = imgRes),
+                contentDescription = null
+            )
+            Button(
+                onClick = { result = (1..6).random() },
+                modifier = Modifier.align(Alignment.CenterHorizontally)
+            ) {
+                Text(text = "Click")
+            }
+        }
     }
 }
 
 
-@Composable
-fun MainSection(modifier: Modifier = Modifier) {
-    Column(
-        modifier = modifier
-    ) {
-        Image(
-            painter = painterResource(id = R.drawable.android_logo),
-            contentDescription = null,
-            modifier = Modifier
-                .width(150.dp)
-                .height(150.dp)
-                .align(Alignment.CenterHorizontally)
-        )
-        Text(
-            text = "ChanHyeok Park",
-            fontSize = 32.sp,
-            fontWeight = FontWeight.Bold,
-            textAlign = TextAlign.Center,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp)
-        )
-        Text(
-            text = "Android Developer",
-            textAlign = TextAlign.Center,
-            fontSize = 16.sp,
-            modifier = Modifier.fillMaxWidth()
-        )
-    }
-}
-
-@Composable
-fun InfoSection(modifier: Modifier = Modifier) {
-    val phone = Icons.Filled.Phone
-    val share = Icons.Filled.Share
-    val mail = Icons.Filled.MailOutline
-    Column(
-        modifier = modifier
-    ) {
-        InfoLine(
-            phone, "+11 (123) 444 555 666",
-            modifier = Modifier.align(Alignment.CenterHorizontally)
-        )
-        InfoLine(
-            share, "@chanhypark",
-            modifier = Modifier.align(Alignment.CenterHorizontally)
-        )
-        InfoLine(
-            mail, "chanhypark@velog.io",
-            modifier = Modifier.align(Alignment.CenterHorizontally)
-        )
-    }
-}
-
-@Composable
-fun InfoLine(icons: ImageVector, content: String, modifier: Modifier = Modifier) {
-    Row(
-        modifier = modifier
-    ) {
-        Icon(imageVector = icons, contentDescription = null, modifier.padding(end = 16.dp))
-        Text(text = content)
-    }
-}
 
 @Preview(showBackground = true)
 @Composable
-fun ContainerPreview() {
+fun DiceImageAndButtonPreview() {
     ComposeCodelabTheme {
-        Container()
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun MainSectionPreview() {
-    ComposeCodelabTheme {
-        MainSection()
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun InfoSectionPreview() {
-    ComposeCodelabTheme {
-        InfoSection()
+        DiceImageAndButton()
     }
 }
