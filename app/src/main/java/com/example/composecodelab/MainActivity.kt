43a50cd58818e63b3c9f7d7f014a0bc172039820
chanhypark <chanhypark@gmarket.com>
2024-01-09 10:28:31 +0900

    [CodeLab Tutorial] Base Practice 'Compose 도움말 UI'

--- app/src/main/java/com/example/composecodelab/MainActivity.kt
+++ app/src/main/java/com/example/composecodelab/MainActivity.kt
@@ -4,13 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -18,9 +16,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -37,57 +35,52 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    GreetingImage(message = "HappyBirthDay Sam", from = "from Code Pilot")
+                    MainContainer()
                 }
             }
         }
     }
 }
-
-
 @Composable
-fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
-    Column(
-        verticalArrangement = Arrangement.Center,
-        modifier = Modifier.fillMaxSize()
-    ) {
-        Text(
-            text = message,
-            fontSize = 100.sp,
-            lineHeight = 100.sp,
-            textAlign = TextAlign.Center,
-            modifier = Modifier.padding(8.dp)
-        )
-        Text(
-            text = from,
-            fontSize = 36.sp,
-            modifier = Modifier
-                .padding(16.dp)
-                .align(alignment = Alignment.CenterHorizontally)
-        )
+fun MainContainer(modifier: Modifier = Modifier) {
+    Column {
+        TextContainer()
     }
 }
 
 
 @Composable
-fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) {
-    val image = painterResource(id = R.drawable.androidparty)
-    Box {
+fun TextContainer(modifier: Modifier = Modifier) {
+    Column {
         Image(
-            painter = image,
+            painter = painterResource(id = R.drawable.bg_compose_background),
             contentDescription = null,
-            contentScale = ContentScale.Crop
+            modifier = Modifier.fillMaxWidth()
         )
-        GreetingText(
-            message = message, from = from, modifier = Modifier.fillMaxSize().padding(8.dp)
+        Text(
+            text = stringResource(id = R.string.title),
+            fontSize = 24.sp,
+            modifier = Modifier.padding(16.dp)
+        )
+        Text(
+            text = stringResource(id = R.string.description),
+            modifier = Modifier
+                .padding(start = 16.dp, end = 16.dp),
+            textAlign = TextAlign.Justify
+        )
+        Text(
+            text = stringResource(id = R.string.content),
+            modifier = Modifier.padding(16.dp).align(Alignment.End),
+            textAlign = TextAlign.Justify
         )
     }
 }
 
-@Preview(showBackground = false)
+
+@Preview(showBackground = true)
 @Composable
-fun BirthDayCardPreview() {
+fun ComposeTutorialPreview() {
     ComposeCodelabTheme {
-        GreetingImage(message = "HBD Sam!", from = "from CodePilot")
+        MainContainer()
     }
 }
