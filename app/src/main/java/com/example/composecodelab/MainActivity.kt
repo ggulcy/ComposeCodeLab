febc294977c6bd08ee7d50209791000c64b950a6
chanhypark <chanhypark@gmarket.com>
2024-01-10 10:03:38 +0900

    [Codelab Tutorial] State를 활용한 팁계산기1

--- app/src/main/java/com/example/composecodelab/MainActivity.kt
+++ app/src/main/java/com/example/composecodelab/MainActivity.kt
@@ -1,17 +1,24 @@
 package com.example.composecodelab
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.annotation.DrawableRes
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -19,9 +26,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.composecodelab.ui.theme.ComposeCodelabTheme
+import java.text.NumberFormat
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,7 +43,7 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
                 ) {
-                    DiceImageAndButton()
+                    CalculationContainer()
                 }
             }
         }
@@ -40,44 +51,62 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun DiceImageAndButton(modifier: Modifier = Modifier) {
-    Box(
-        Modifier.fillMaxSize()
-    ) {
-        var result by remember { mutableStateOf(1) }
-        val imgRes = when (result) {
-            1 -> R.drawable.dice_1
-            2 -> R.drawable.dice_2
-            3 -> R.drawable.dice_3
-            4 -> R.drawable.dice_4
-            5 -> R.drawable.dice_5
-            else -> R.drawable.dice_6
-        }
-        //뒤의 동작에서 result값이 변함에 따라 imgRes도 재정의 된다.
+fun CalculationContainer(modifier: Modifier = Modifier) {
+    var amountInput by remember { mutableStateOf("") }
+    val tip = calculateTip(amountInput.toDoubleOrNull() ?: 0.0)
+    Box(Modifier.fillMaxSize()) {
         Column(
             modifier = Modifier
-                .align(Alignment.Center)
+                .statusBarsPadding()
+                .padding(horizontal = 40.dp)
+                .verticalScroll(rememberScrollState())
+                .safeDrawingPadding()
+                .align(Alignment.CenterStart)
         ) {
-            Image(
-                painter = painterResource(id = imgRes),
-                contentDescription = null
+            Text(
+                text = "Calculate Title"
             )
-            Button(
-                onClick = { result = (1..6).random() },
-                modifier = Modifier.align(Alignment.CenterHorizontally)
-            ) {
-                Text(text = "Click")
-            }
+            EditNumberField(amountInput, {
+                amountInput = it
+            })
+            Text(
+                text = "Tip Amount:$tip",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier
+                    .padding(top = 10.dp)
+                    .align(Alignment.CenterHorizontally)
+            )
         }
     }
 }
 
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EditNumberField(
+    amountInput: String,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    TextField(
+        value = amountInput,
+        onValueChange = onValueChange,
+        modifier = modifier.padding(top = 8.dp, bottom = 8.dp),
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+        singleLine = true,
+        label = { Text(text = "Bill Amount") }
+    )
+}
+
+fun calculateTip(amount: Double, tipPercent: Double = 15.0): String {
+    return NumberFormat.getCurrencyInstance().format(tipPercent / 100 * amount)
+}
 
 @Preview(showBackground = true)
 @Composable
-fun DiceImageAndButtonPreview() {
+fun CalculationContainerPreview() {
     ComposeCodelabTheme {
-        DiceImageAndButton()
+        CalculationContainer()
     }
 }
